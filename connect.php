4c7a09ed6d6d951e26b1f190b9953656ba10a2dc
Pigura1 <C.victor2006@laposte.net>
2025-12-02 09:26:54 +0100

    ajout connect php

---
+++ connect.php
@@ -0,0 +1,12 @@
+
+
+
+<?php
+//c'est pour ce connecter a la base de données , mettez require_once 'connect.php' si vous avez besoin de la base 
+$host = 'localhost'; //Hébergement local
+$dbname = 'photodata';
+$username = 'root';
+$password = ''; //Par défault sur MAMP
+
+
+?>
