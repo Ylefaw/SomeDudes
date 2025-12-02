8fe25c778ec88574fe0b0155d7cb3eb169312ffa
Pigura1 <C.victor2006@laposte.net>
2025-12-02 09:27:19 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -8,5 +8,6 @@ $dbname = 'photodata';
 $username = 'root';
 $password = ''; //Par défault sur MAMP
 
-
+//la table qui contient les informations des photos s'appelle photos
+// ces éléments sont id (automatique Primary Key),nom (le nom de la personne qui donne les photos ), adresse du fichier stocké ( adresse du fichier dans votre pc) et un mail 
 ?>
