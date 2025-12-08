dd094fba304db0bccd309068c845c6bd54ec065a
Jean-Louis Noah <noahbusinessproject.jl@gmail.com>
2025-12-08 10:27:41 +0100

    cookies

--- connect.php
+++ connect.php
@@ -4,3 +4,23 @@ $host = 'localhost';
 $dbname = 'photodata';
 $username = 'root';
 $password = '';
+
+// Récupérer l'adresse IP du visiteur
+$ip = $_SERVER['REMOTE_ADDR'];
+
+
+$cookievalue = $ip;
+
+
+setcookie(
+    'liste_ip',              // nom du cookie
+    $cookievalue,            // valeur du cookie
+    time() + 12*24*3600,     // expiration (12 jours)
+    '/',                     // chemin (disponible sur tout le site)
+    '',                      // domaine (vide = domaine courant)
+    false,                   // sécurisé (true = HTTPS uniquement)
+    true                     // HttpOnly (empêche accès via JS)
+);
+
+// Vérification
+echo "Cookie créé avec l'adresse IP : " . $cookievalue;
