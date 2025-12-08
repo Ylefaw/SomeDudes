f08add184ff8b1c70de4597bc6e17bd8554de140
Pigura1 <C.victor2006@laposte.net>
2025-12-08 09:32:54 +0100

    ne pas commit test perso

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 
 <?php
 // Connexion à la base
-$host = 'localhost';
-$dbname = 'photodata';
-$username = 'root';
-$password = '';
+$host = 'mysql-testurururu.alwaysdata.net';
+$dbname = 'testurururu_somedudes';
+$username = '443944';
+$password = 'tkD:L!]B9@>y!Gk';
